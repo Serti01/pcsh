434c89e42e9eb5f9c46a1b6d4d89ce948bdbd8b7
Samoyo <samoyo@protonmail.com>
2021-07-03 09:17:58 -0400

    Update lib/*

--- lib/pcsh.h
+++ lib/pcsh.h
@@ -1,5 +1,19 @@
 #pragma once
 
-int cd(char *path);
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
 
+int cd(char *path);
 int cmpstr(char *s1, char *s2);
+int getcursorpos(int *const row, int *const col);
+
+#define VERSION "pcsh 0.2.0-alpha"
+
+// TODO: add config file
+#define SCROLLBACK_LIMIT 128
